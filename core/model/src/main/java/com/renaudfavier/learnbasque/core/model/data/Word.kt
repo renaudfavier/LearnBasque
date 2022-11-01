35338df697a7349d73a183394013c9e498b2c16f
Renaud Favier <renaud.favier@360learning.com>
2022-11-01 23:17:36 +0100

    Add model module with its first class : Word

---
+++ core/model/src/main/java/com/renaudfavier/learnbasque/core/model/data/Word.kt
@@ -0,0 +1,8 @@
+package com.renaudfavier.learnbasque.core.model.data
+
+data class Word(
+    val id: String,
+    val frenchTranslation: String,
+    val basqueTranslation: String,
+)
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "learnbasque"
 include(":app")
+include(":core:model")
