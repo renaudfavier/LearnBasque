05f5f69863a8c6389720f98a825beb1581d09fe7
Renaud Favier <renaud.favier@360learning.com>
2022-11-07 21:44:30 +0100

    Improve Memory test design

--- feature/vocabulary/src/main/java/com/renaudfavier/learnbasque/feature/vocabulary/VocabularyScreen.kt
+++ feature/vocabulary/src/main/java/com/renaudfavier/learnbasque/feature/vocabulary/VocabularyScreen.kt
@@ -1,23 +1,27 @@
 package com.renaudfavier.learnbasque.feature.vocabulary
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.renaudfavier.learnbasque.core.designsystem.theme.LearnBasqueTheme
+import com.renaudfavier.learnbasque.core.designsystem.theme.PurpleGrey80
+import com.renaudfavier.learnbasque.core.designsystem.theme.Typography
 
 @Composable
 fun VocabularyScreen(
@@ -26,19 +30,28 @@ fun VocabularyScreen(
 ) {
     val vocabularyUiState = viewModel.vocabularyUiState.collectAsState()
 
-    when(val value = vocabularyUiState.value) {
-        VocabularyUiState.Loading -> {
-            CircularProgressIndicator()
-        }
-        VocabularyUiState.Error -> {
-            Text(text = "error")
-        }
-        is VocabularyUiState.EasyMemoryCardUiModel -> {
-            EasyMemoryCard(
-                model = value,
-                onProp1Click = viewModel::answerProposition1,
-                onProp2Click = viewModel::answerProposition2,
-            )
+    Surface(
+        modifier = modifier,
+        color = PurpleGrey80
+    ) {
+        Box(modifier = modifier,
+            contentAlignment = Alignment.Center
+        ) {
+            when(val value = vocabularyUiState.value) {
+                VocabularyUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                VocabularyUiState.Error -> {
+                    Text(text = "error")
+                }
+                is VocabularyUiState.EasyMemoryCardUiModel -> {
+                    EasyMemoryCard(
+                        model = value,
+                        onProp1Click = viewModel::answerProposition1,
+                        onProp2Click = viewModel::answerProposition2,
+                    )
+                }
+            }
         }
     }
 }
@@ -48,16 +61,30 @@ fun EasyMemoryCard(
     model: VocabularyUiState.EasyMemoryCardUiModel,
     onProp1Click: () -> Unit,
     onProp2Click: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Card(
-        colors = CardDefaults.cardColors(containerColor = model.backgroundColor ?: Color.White)
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = model.backgroundColor ?: Color.White),
     ) {
-        Text(text = model.wordToTranslate)
-        Button(onClick = onProp1Click) {
-            Text(text = model.proposition1)
-        }
-        Button(onClick = onProp2Click) {
-            Text(text = model.proposition2)
+        Text(
+            text = model.wordToTranslate,
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 24.dp),
+            style = Typography.displayMedium,
+        )
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .padding(bottom = 12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = onProp1Click) {
+                Text(text = model.proposition1)
+            }
+            Button(onClick = onProp2Click,
+            modifier = Modifier.padding(start = 12.dp)) {
+                Text(text = model.proposition2)
+            }
         }
     }
 }
@@ -66,17 +93,23 @@ fun EasyMemoryCard(
 @Composable
 fun MemoryCardPreview() {
     LearnBasqueTheme {
-        EasyMemoryCard(
-            model = fakeCardModel,
-            onProp1Click = {},
-            onProp2Click = {},
-        )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = PurpleGrey80
+        ) {
+            EasyMemoryCard(
+                model = fakeCardModel,
+                onProp1Click = {},
+                onProp2Click = {},
+            )
+        }
     }
 }
 
+
 private val fakeCardModel = VocabularyUiState.EasyMemoryCardUiModel(
     id = "",
-    wordToTranslate = "motBasque",
-    proposition2 = "prop1",
-    proposition1 = "prop1"
+    wordToTranslate = "Milesker Anitz",
+    proposition1 = "Peut-être",
+    proposition2 = "Merci beaucoup",
 )
