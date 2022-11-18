579c2f4b90c0dac0b1a74fa492372d918ba4c427
Renaud Favier <renaud.favier@360learning.com>
2022-11-18 16:05:45 +0100

    Create GetBestExerciseToTryNext UseCase

--- core/domain/src/main/java/com/renaudfavier/learnbasque/core/domain/GetBestExerciseToTryNextUseCase.kt
+++ core/domain/src/main/java/com/renaudfavier/learnbasque/core/domain/GetBestExerciseToTryNextUseCase.kt
@@ -1,9 +1,15 @@
 package com.renaudfavier.learnbasque.core.domain
 
 import com.renaudfavier.learnbasque.core.data.repository.UserAnswerRepository
+import com.renaudfavier.learnbasque.core.data.repository.WordsRepository
+import com.renaudfavier.learnbasque.core.domain.model.KnowledgeWithMastering
+import com.renaudfavier.learnbasque.core.model.data.Exercise
+import com.renaudfavier.learnbasque.core.model.data.Knowledge
 import javax.inject.Inject
 
 class GetBestExerciseToTryNextUseCase @Inject constructor(
+    private val wordsRepository: WordsRepository,
+    private val getUserKnowledgeSetUseCase: GetUserKnowledgeSetUseCase,
     private val answerRepository: UserAnswerRepository,
 ) {
 
@@ -18,9 +24,36 @@ class GetBestExerciseToTryNextUseCase @Inject constructor(
         - The right knowledge to reinforce is the one that will be cost efficient to reinforce
      */
 
-    private suspend fun getKnowledgeSet() {
+    suspend operator fun invoke(): Exercise {
 
+        val userKnowledge = getUserKnowledgeSetUseCase()
+
+        val bestExercise = userKnowledge
+            .map { Pair(it, it.getBestExercise()) }
+            .filter { (userKnowledge, bestExercise) ->
+                bestExercise.maxMastering() - (userKnowledge.mastering ?: 0f) > 0.05f
+            }.maxByOrNull { (userKnowledge, bestExercise) ->
+                bestExercise.maxMastering() - (userKnowledge.mastering ?: 0f)
+            }
+            ?.second
+
+        return bestExercise ?: run {
+            val newWord = wordsRepository.getWords()
+                .filter { word -> userKnowledge.none { it.knowledge == Knowledge.Vocabulary(word) } }
+                .random()
+            Exercise.NewWord(newWord.id)
+        }
     }
 
+    private fun KnowledgeWithMastering.getBestExercise() = when (this.knowledge) {
+        is Knowledge.Vocabulary -> knowledge.getBestVocabularyExercise(this.mastering)
+    }
+
+    private fun Knowledge.Vocabulary.getBestVocabularyExercise(mastering: Float?) = when {
+        mastering == null -> Exercise.NewWord(this.word.id)
+        mastering < 0.08f -> Exercise.NewWord(this.word.id)
+        mastering < 0.15f -> Exercise.TranslateFromBasque(this.word.id, Exercise.TranslateFromBasque.Difficulty.TwoPropositions)
+        else ->  Exercise.TranslateToBasque(this.word.id, Exercise.TranslateToBasque.Difficulty.TwoPropositions)
+    }
 
 }
---
+++ core/domain/src/test/java/com/renaudfavier/learnbasque/core/domain/GetBestExerciceToTryNextUseCaseTest.kt
@@ -0,0 +1,109 @@
+package com.renaudfavier.learnbasque.core.domain
+
+import com.renaudfavier.learnbasque.core.data.repository.IdBasedExerciseRepository
+import com.renaudfavier.learnbasque.core.domain.model.KnowledgeWithMastering
+import com.renaudfavier.learnbasque.core.model.data.Exercise
+import com.renaudfavier.learnbasque.core.model.data.Exercise.TranslateFromBasque.Difficulty.TwoPropositions
+import com.renaudfavier.learnbasque.core.model.data.Knowledge
+import com.renaudfavier.learnbasque.core.model.data.QuestionAnswer
+import com.renaudfavier.learnbasque.core.model.data.UserAnswer
+import com.renaudfavier.learnbasque.core.model.data.Word
+import com.renaudfavier.learnbasque.core.testing.model.testNewWordExercise
+import com.renaudfavier.learnbasque.core.testing.model.testTranslateFromBasqueExercise
+import com.renaudfavier.learnbasque.core.testing.model.testTranslateToBasqueExercise
+import com.renaudfavier.learnbasque.core.testing.model.testUserAnswer
+import com.renaudfavier.learnbasque.core.testing.model.testWord
+import com.renaudfavier.learnbasque.core.testing.repository.TestUserAnswerRepository
+import com.renaudfavier.learnbasque.core.testing.repository.TestWordsRepository
+import com.renaudfavier.learnbasque.core.testing.util.MainDispatcherRule
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import org.hamcrest.MatcherAssert
+import org.junit.Assert.assertArrayEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
+
+class GetBestExerciceToTryNextUseCaseTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val wordsRepository = TestWordsRepository()
+    private val userAnswerRepository = TestUserAnswerRepository()
+    private val exerciseRepository = IdBasedExerciseRepository()
+    private val getUserKnowledgeSetUseCase = GetUserKnowledgeSetUseCase(
+        exerciseRepository, userAnswerRepository, wordsRepository
+    )
+
+    val useCase = GetBestExerciseToTryNextUseCase(
+        wordsRepository = wordsRepository,
+        getUserKnowledgeSetUseCase = getUserKnowledgeSetUseCase,
+        answerRepository = userAnswerRepository,
+    )
+
+    @Test
+    fun whenNoData_NewWordIsReturned() = runTest {
+
+        val sampleWord1 = testWord(id = "1")
+        wordsRepository.setupNextWords(listOf(sampleWord1))
+        userAnswerRepository.setupNextAnswers(emptyList())
+
+        val result = useCase()
+
+        assertEquals(result, Exercise.NewWord("1"))
+    }
+
+    @Test
+    fun whenFailedAnswer_SameAnswerIsProposed() = runTest {
+
+         val sampleWord1 = testWord(id = "1")
+         val sampleWord2 = testWord(id = "2")
+        wordsRepository.setupNextWords(listOf(sampleWord1, sampleWord2))
+
+         val sampleNewWord1 = testNewWordExercise(sampleWord1.id)
+         val sampleNewWord2 = testNewWordExercise(sampleWord2.id)
+         val sampleExercise1 = testTranslateFromBasqueExercise(sampleWord1.id)
+         val sampleExercise2 = testTranslateFromBasqueExercise(sampleWord2.id)
+
+        val userAnswers = listOf(
+            testUserAnswer(questionId = sampleNewWord1.id, isCorrect = true),
+            testUserAnswer(questionId = sampleNewWord2.id, isCorrect = true),
+            testUserAnswer(questionId = sampleExercise1.id, isCorrect = true),
+            testUserAnswer(questionId = sampleExercise1.id, isCorrect = true),
+            testUserAnswer(questionId = sampleExercise1.id, isCorrect = true),
+            testUserAnswer(questionId = sampleExercise2.id, isCorrect = false),
+            testUserAnswer(questionId = sampleExercise2.id, isCorrect = true),
+        )
+        userAnswerRepository.setupNextAnswers(userAnswers)
+
+
+        val result = useCase()
+
+        assertEquals(testTranslateToBasqueExercise(sampleWord1.id), result)
+    }
+
+    @Test
+    fun whenKnowledgeIsMaxed_NewWord() = runTest {
+
+        val sampleWord1 = testWord(id = "1")
+        val sampleWord2 = testWord(id = "2")
+        wordsRepository.setupNextWords(listOf(sampleWord1, sampleWord2))
+
+        // Most difficult Exercise
+        val sampleExercise1 = testTranslateToBasqueExercise(sampleWord1.id)
+
+        // User last 3 answers were correct to the most difficult exercise
+        val sampleCorrectAnswer = testUserAnswer(questionId = sampleExercise1.id, isCorrect = true)
+        userAnswerRepository.setupNextAnswers(listOf(sampleCorrectAnswer, sampleCorrectAnswer, sampleCorrectAnswer))
+
+        val result = useCase()
+
+        // We return a new word
+        assertEquals(Exercise.NewWord(sampleWord2.id), result)
+    }
+
+}
