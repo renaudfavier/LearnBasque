379665da0256ee3ff46b35c3fd441e54ca5f159e
Renaud Favier <renaud.favier@360learning.com>
2022-11-22 01:04:56 +0100

    Add Id util class

---
+++ core/model/src/main/java/com/renaudfavier/learnbasque/core/model/data/util/Id.kt
@@ -0,0 +1,20 @@
+package com.renaudfavier.learnbasque.core.id
+
+data class Id<Type>(val raw: String) {
+
+    override fun toString() = raw
+
+    override fun equals(other: Any?): Boolean {
+        return raw == other || raw == (other as? Id<*>)?.raw
+    }
+
+    override fun hashCode(): Int {
+        return raw.hashCode()
+    }
+}
+
+fun <Type> String.toId(): Id<Type> = Id(this)
+fun <Type> Iterable<String>.toIds(): List<Id<Type>> = map(String::toId)
+fun <Type> Iterable<String>.toIdSet(): Set<Id<Type>> = mapTo(mutableSetOf(), String::toId)
+val Iterable<Id<*>>.raws get() = map { it.raw }
+val Iterable<Id<*>>.rawSet get() = mapTo(mutableSetOf(), { it.raw })
